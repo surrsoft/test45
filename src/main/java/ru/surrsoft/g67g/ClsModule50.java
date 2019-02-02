7b13d4d969eb3e7574c9fa7d76fa87dbe72b7f07
surr <e5535@mail.ru>
2019-02-02 22:43:18 +0300

    initial commit

---
+++ src/main/java/ru/surrsoft/g67g/ClsModule50.java
@@ -0,0 +1,20 @@
+package ru.surrsoft.g67g;
+
+/**
+ * //needdesc
+ * <p>
+ * ВЕРСИЯ 1 1.0 2019-02-02 //rgx new
+ */
+public class ClsModule50 {
+  private final String st;
+  private final String stx;
+  private String sty;
+  private String stz;
+
+  public ClsModule50(String st, String stx, String sty, String stz) {
+    this.st = st;
+    this.stx = stx;
+    this.sty = sty;
+    this.stz = stz;
+  }
+}
